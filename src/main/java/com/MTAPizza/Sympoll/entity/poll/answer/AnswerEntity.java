6e5952a5be8b34b806c1174d8b8af8de05e4f793
Ronenii <ronen.glm@gmail.com>
2024-05-13 16:03:16 +0300

    - Added AnswerEntity.java

---
+++ src/main/java/com/MTAPizza/Sympoll/entity/poll/answer/AnswerEntity.java
@@ -0,0 +1,55 @@
+package com.MTAPizza.Sympoll.entity.poll.answer;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "answer_options")
+public class AnswerEntity implements Serializable {
+    @Id
+    @Column(name = "answer_id", nullable = false)
+    private int answerID;
+    @Column(name = "poll_id", nullable = false)
+    private int pollID;
+    @Column(name = "ordinal", nullable = false)
+    private int ordinal;
+    @Column(name = "answer_text")
+    private String answerText;
+    @Column(name = "num_of_votes")
+    private int numOfVotes;
+
+    public AnswerEntity() {
+    }
+
+    public AnswerEntity(int answerID, int pollID, int ordinal, String answerText, int numberOfVotes) {
+        this.answerID = answerID;
+        this.pollID = pollID;
+        this.ordinal = ordinal;
+        this.answerText = answerText;
+        this.numOfVotes = numberOfVotes;
+    }
+
+    public int getAnswerID() {
+        return answerID;
+    }
+
+    public int getPollID() {
+        return pollID;
+    }
+
+    public int getOrdinal() {
+        return ordinal;
+    }
+
+    public String getAnswerText() {
+        return answerText;
+    }
+
+    public int getNumOfVotes() {
+        return numOfVotes;
+    }
+}
